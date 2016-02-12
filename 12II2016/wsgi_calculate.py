4e4065adcf5b789726cab4d0d00c1457196bebdb
Petro-Y <python@i.ua>
2016-02-12 20:32:09 +0200

    wsgi_calculate: simple wsgi app. parentheses supported

--- 12II2016/wsgi_calculate.py
+++ 12II2016/wsgi_calculate.py
@@ -3,11 +3,23 @@ from urllib.parse import parse_qs
 #from html import escape
 import re
 
+def check_parentheses(expr):
+    expr=re.sub(r'[^()]', '', expr)
+    lvl=0
+    for c in expr:
+        if c=='(':
+            lvl+=1
+        elif c==')':
+            lvl-=1
+        if lvl<0:
+            return False
+    return lvl==0
+
 def app(environ, start_response):
     parameters = parse_qs(environ.get('QUERY_STRING', ''))
     expr=parameters['expr'][0] if 'expr' in parameters else ''
     # verify expr using regex:
-    if not re.fullmatch(r'[0-9]+([-+*/][0-9]+)+',expr):
+    if not re.fullmatch(r'\(*-?[0-9]+([-+*/]\(*-?[0-9]+\)*)*\)*',expr) or not check_parentheses(expr):
         expr=''
     start_response('200 OK', [('Content-Type', 'text/html')])
     return [
